b1b7920faeb85cb102531c73c8e07202ed39c033
mayuelcuarto <cristhianmayuri@gmail.com>
2018-01-22 19:49:01 -0500

    Configuración de Rutas Yml
    Configuración de directorios y ficheros para realizar la configuración de rutas mediante archivos YML

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+    
+    public function pruebasAction() {
+        echo "Hola Mundo con Symfony 3!";
+        die();
     }
 }
