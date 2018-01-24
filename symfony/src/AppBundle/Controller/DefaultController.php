bbcd21c8743d96443cdb8de605a3910fc1a54714
mayuelcuarto <cristhianmayuri@gmail.com>
2018-01-24 01:01:16 -0500

    Añadiendo loginAction
    Mediante el uso de Symfony\Component\Validator\Constraints, se realiza la validación del email.

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints as Assert;
 use AppBundle\Services\Helpers;
 
 class DefaultController extends Controller
@@ -16,6 +17,46 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+    
+    public function loginAction(Request $request){
+        $helpers = $this->get(Helpers::class);
+        
+        // Recibir json por POST
+        $json = $request->get('json', null);
+        
+        //Array a devolver por defecto
+        $data = array(
+            'status' => 'error',
+            'data' => 'Send json via POST'
+        );
+        
+        if($json != null){
+            // Me haces el login
+            
+            //Convertimos un json a un objeto de PHP
+            $params = json_decode($json);
+            
+            $email = (isset($params->email)) ? $params->email : null;
+            $password = (isset($params->password)) ? $params->password : null;
+            
+            $emailConstraint = new Assert\Email();
+            $emailConstraint->message = "Este email no es válido";
+            $validate_email = $this->get("validator")->validate($email, $emailConstraint);
+            
+            if(count($validate_email) == 0 && $password != null){
+                $data = array(
+                    'status' => 'success',
+                    'data' => 'Login exitoso'
+                );
+            }else{
+                $data = array(
+                    'status' => 'success',
+                    'data' => 'Email o Password incorrecto'
+                );
+            }    
+        }
+        return $helpers->json($data) ;
     }
     
     public function pruebasAction() {
