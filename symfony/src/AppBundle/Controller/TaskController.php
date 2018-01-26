8ff7dac9a6f36f8e9d538c0ca45ba9a9716ef1cc
mayuelcuarto <cristhianmayuri@gmail.com>
2018-01-26 12:35:17 -0500

    Paginación de Tareas
    Programación de la función tasksAction, creación de la ruta task_list en el archivo task.yml.

--- symfony/src/AppBundle/Controller/TaskController.php
+++ symfony/src/AppBundle/Controller/TaskController.php
@@ -106,4 +106,44 @@ class TaskController extends Controller{
         
         return $helpers->json($data);
     }
+    
+    public function tasksAction(Request $request){
+        $helpers = $this->get(Helpers::class);
+        $jwt_auth = $this->get(JwtAuth::class);
+        
+        $token = $request->get('authorization',null);
+        $authCheck = $jwt_auth->checkToken($token);
+        
+        if($authCheck==true){
+            $identity = $jwt_auth->checkToken($token, true);
+            $em = $this->getDoctrine()->getManager();
+            
+            $dql = "SELECT t FROM BackendBundle:Task t ORDER BY t.id DESC";
+            $query = $em->createQuery($dql);
+            
+            $page = $request->query->getInt('page', 1);
+            $paginator = $this->get('knp_paginator');
+            $items_per_page = 10;
+            
+            $pagination = $paginator->paginate($query, $page, $items_per_page);
+            $total_items_count = $pagination->getTotalItemCount();
+            
+            $data = array(
+                "status" => "Succes",
+                "code" => 200,
+                "total_items_count" => $total_items_count,
+                "page_actual" => $page,
+                "items_per_page" => $items_per_page,
+                "total_pages" => ceil($total_items_count/$items_per_page),
+                "data" => $pagination
+            );
+        }else{
+            $data = array(
+                "status" => "Error",
+                "code" => 400,
+                "msg" => "Autorización no Válida!!"
+            );
+        }
+        return $helpers->json($data);
+    }
 }
